5a3bf2d6de579718fb99650686d1721610b37b19
Bill <wjquayle@gmail.com>
2023-07-11 13:14:32 -0500

    completed lab

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function moveDodgerRight(){
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
   
-    if (left > 0) {
+    if (left < 360) {
       dodger.style.left = `${left + 1}px`;
     }
 }
